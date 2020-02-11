d3db5792fd189e4cd24e32c4b50c31ffff85dfac
Steven Lam <stevenvl@gmail.com>
2020-02-11 03:33:42 -0500

    table/array for questions added/modifying spacing rows/col

---
+++ js/script.js
@@ -0,0 +1,8 @@
+  // The array of questions for our quiz game.
+  var questions = [
+    { q: " ", a: " " },
+    { q: " ", a: " " },
+    { q: " ", a: " " },
+    { q: " ", a: "" },
+    { q: " ", a: " " }
+  ];
