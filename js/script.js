ee26726213fbf2804b961b597878f0263ac71ce3
Steven Lam <stevenvl@gmail.com>
2020-02-13 00:12:31 -0500

    responsive html layout finished/buttons created/array created

--- js/script.js
+++ js/script.js
@@ -1,8 +1,33 @@
-  // The array of questions for our quiz game.
-  var questions = [
-    { q: " ", a: " " },
-    { q: " ", a: " " },
-    { q: " ", a: " " },
-    { q: " ", a: "" },
-    { q: " ", a: " " }
-  ];
+  // The array of questions for our quiz game
+  
+  let quizQuestionsArray = [
+    {
+        Question:"Inside which HTML element do we put the JavaScript?",
+        answerA: "HTML",
+        answerB: "JavaScript",
+        answerC: "CSS",
+        answerB: "Bootstrap",
+    },
+    {
+        Question:"Inside which HTML element do we put the JavaScript?",
+        answerA: "HTML",
+        answerB: "JavaScript",
+        answerC: "CSS",
+        answerB: "Bootstrap",
+    },
+    {
+        Question:"Inside which HTML element do we put the JavaScript?",
+        answerA: "HTML",
+        answerB: "JavaScript",
+        answerC: "CSS",
+        answerB: "Bootstrap",
+    },
+    {
+        Question:"Inside which HTML element do we put the JavaScript?",
+        answerA: "HTML",
+        answerB: "JavaScript",
+        answerC: "CSS",
+        answerB: "Bootstrap",
+    }
+
+  ]
